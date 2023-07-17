e0c45de58c24a4cc9e4c5ec6b85cc7af2b1b8fa7
Heles <heles.hals@gmail.com>
2023-07-17 15:28:22 +0300

    Add project files.

---
+++ Student&Aspirant/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class Student {
+protected:
+    std::string name;
+    int age;
+
+public:
+    Student(const std::string& name, int age) : name(name), age(age) {}
+
+    void display() const {
+        std::cout << "Student Name: " << name << std::endl;
+        std::cout << "Age: " << age << std::endl;
+    }
+};
+
+class Aspirant : public Student {
+private:
+    std::string researchTopic;
+
+public:
+    Aspirant(const std::string& name, int age, const std::string& researchTopic)
+        : Student(name, age), researchTopic(researchTopic) {}
+
+    void display() const {
+        Student::display();
+        std::cout << "Research Topic: " << researchTopic << std::endl;
+    }
+};
+
+int main() {
+    Student student("John Doe", 20);
+    student.display(); // Виведе інформацію про студента
+
+    std::cout << "----------------------" << std::endl;
+
+    Aspirant aspirant("Jane Smith", 25, "Machine Learning");
+    aspirant.display(); // Виведе інформацію про аспіранта
+
+    return 0;
+}
